58fe11169911997a87edea316716a3981f10f8e2
Jon Geater <jon.geater@rkvst.com>
2024-11-21 15:03:13 +0100

    Convert input to Register Signed Statement to pycose structured type
    Signed-off-by: Jon Geater <jon.geater@datatrails.ai>

--- tests/test_datatrails.py
+++ tests/test_datatrails.py
@@ -5,6 +5,7 @@ import os
 import sys
 import time
 
+from pycose.messages import Sign1Message
 from py_scrapi.py_scrapi import PyScrapi
 
 args = {
@@ -18,9 +19,12 @@ print("Initializing TS connection")
 myScrapi = PyScrapi("DataTrails", args)
 
 print("Registering Signed Statement")
-# Read the binary data from the file
+# Read the binary data from the file and make into Sign1Message
 with open("signed-statement.cbor", "rb") as data_file:
-    original_signed_statement = data_file.read()
+    original_cose = data_file.read()
+
+    original_signed_statement = Sign1Message.decode(original_cose)
+
     # Send to SCITT Transparency Service
     lro = myScrapi.register_signed_statement(original_signed_statement)
     if not lro:
@@ -69,7 +73,7 @@ retrieved_signed_statement = myScrapi.resolve_signed_statement(
 pprint(retrieved_signed_statement)
 
 retrieved_cose = retrieved_signed_statement.encode(tag=True, sign=False)
-if original_signed_statement != retrieved_cose:
+if original_cose != retrieved_cose:
     print("FATAL: STATEMENTS DO NOT MATCH!")
     sys.exit(1)
 
